dd53da1487046af4b484a5b2024ab88544503b29
Neeson Zhang <neesonqk@gamil.com>
2017-10-27 16:11:40 +0800

    Added travis

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/pac/pac.min.js');
+	res.sendFile(__dirname + '/pac/pac.js');
 })
 
 app.listen(3000);
